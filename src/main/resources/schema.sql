f1cdb98ba0141f466a8cbd6be29d948f11a22051
LeonidBS <l.kim4p@yandex.ru>
2023-07-16 21:20:11 +0300

    features. Tests. Refactor Sprint#15 .  Iteration 1

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,12 @@
+drop table IF EXISTS users cascade ;
+
+drop table IF EXISTS items cascade ;
+
+drop table IF EXISTS bookings;
+
+drop table  IF EXISTS comments;
+
+drop table IF EXISTS item_requests;
 
 CREATE TABLE IF NOT EXISTS users
 (
